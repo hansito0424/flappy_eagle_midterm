3c888467af2541bc00bc5c6eb96fe1bae53921e4
Hans Vincent Custodio <hcustodio_21ur1517@psu.edu.ph>
2024-04-01 13:06:39 +0800

    first commit

---
+++ lib/game/assets.dart
@@ -0,0 +1,19 @@
+class Assets {
+  static const backgorund = 'background_rizal.png';
+  static const ground = 'grounds.png';
+  static const clouds = 'clouds.png';
+  static const pipe = 'flag.png';
+  static const pipeRotated = 'flag_rotate.png';
+
+  static const birdMidFlap = 'eagles_mid.png';
+  static const birdUpFlap = 'eagles_up.png';
+  static const birdDownFlap = 'eagles_down.png';
+
+  static const gameOver = 'assets/images/gameover.png';
+  static const menu = 'assets/images/menu_screen.jpg';
+  static const message = 'assets/images/meessage.jpg';
+
+  static const flying = 'fly.wav';
+  static const collision = 'collision.wav';
+  static const point = 'point.wav';
+}
